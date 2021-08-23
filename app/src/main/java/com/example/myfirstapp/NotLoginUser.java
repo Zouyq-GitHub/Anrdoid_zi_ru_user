900e31bbde8badc728f15e326a07e1f0517a414c
Zouyq-GitHub <1668751790@qq.com>
2021-08-23 17:38:59 +0800

    push 8.23

--- app/src/main/java/com/example/myfirstapp/NotLoginUser.java
+++ app/src/main/java/com/example/myfirstapp/NotLoginUser.java
@@ -2,16 +2,27 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class NotLoginUser extends AppCompatActivity {
 
@@ -28,22 +39,86 @@ public class NotLoginUser extends AppCompatActivity {
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String FILENAME = "hello_file.txt";
-                String string = "hello world!";
+                Log.e("是否点击进入","true");
+                String filename = "myfile";
+                String fileContents = "Hello world!";
+//                try (FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE)) {
+//                    fos.write(fileContents.toByteArray());
+//                }
 
-                FileOutputStream fos = null;
-                try {
-                    fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
-                    fos.write(string.getBytes());
-                    fos.close();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Log.e("test","demo1");
             }
         });
-
     }
+
+////    public static String rootXMLPath = Environment.getExternalStorageDirectory() + "/testTXT";
+//        public static String rootXMLPath = "/storage/emulated/0";
+//    /**
+//     * 保存内容到TXT文件中
+//     *
+//     * @param fileName
+//     * @param content
+//     * @return
+//     */
+//    public static boolean writeToXML(String fileName, String content) {
+//        Log.e("path:",rootXMLPath);
+//        FileOutputStream fileOutputStream;
+//        BufferedWriter bufferedWriter;
+//        createDirectory(rootXMLPath);
+//        File file = new File(rootXMLPath + "/" + fileName + ".txt");
+//        try {
+//            file.createNewFile();
+//            fileOutputStream = new FileOutputStream(file);
+//            bufferedWriter = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
+//            bufferedWriter.write(content);
+//            bufferedWriter.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//        return true;
+//    }
+//    /**
+//     * 读取XML内容
+//     *
+//     * @param filePath
+//     * @return
+//     */
+//    public static String readFromXML(String filePath) {
+//        FileInputStream fileInputStream;
+//        BufferedReader bufferedReader;
+//        StringBuilder stringBuilder = new StringBuilder();
+//        File file = new File(filePath);
+//        if (file.exists()) {
+//            try {
+//                fileInputStream = new FileInputStream(file);
+//                bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+//                String line;
+//                while ((line = bufferedReader.readLine()) != null) {
+//                    stringBuilder.append(line);
+//                }
+//                bufferedReader.close();
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//                return null;
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//                return null;
+//            }
+//        }
+//        return stringBuilder.toString();
+//    }
+//
+//
+//
+//    /**
+//     * 创建文件夹
+//     *
+//     * @param fileDirectory
+//     */
+//    public static void createDirectory(String fileDirectory) {
+//        File file = new File(fileDirectory);
+//        if (!file.exists()) {
+//            file.mkdirs();
+//        }
+//    }
 }
