1fbd1693f8394ff39b403ff21023235eb096b9bc
ZouyqGitHub <1668751790@qq.com>
2021-08-27 15:17:53 +0800

    push 8.27 05  退出登录逻辑完成 准备实现弹出选择框

--- app/src/main/java/com/example/myfirstapp/UserSetting.java
+++ app/src/main/java/com/example/myfirstapp/UserSetting.java
@@ -2,11 +2,17 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.myfirstapp.entity.User;
+
+import java.io.FileOutputStream;
 
 public class UserSetting extends AppCompatActivity {
 
@@ -14,6 +20,7 @@ public class UserSetting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_setting);
+        LogOut();
     }
 
     @Override
@@ -28,5 +35,31 @@ public class UserSetting extends AppCompatActivity {
             LinearLayout layout = findViewById(R.id.notLogin);
             layout.setVisibility(View.VISIBLE);
         }
+    }
+
+    //退出登录逻辑
+    private void LogOut(){
+        //获取文本ID触发点击事件
+        TextView textView = findViewById(R.id.LogOut);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("点击退出登录","true");
+                try {
+                    //清空文档
+                    FileOutputStream outputStream = openFileOutput("login.txt", Context.MODE_PRIVATE);
+                    //数据录入-刷新
+                    outputStream.write("".getBytes());
+                    outputStream.flush();
+                    //页面跳用户主页
+                    Intent intent = new Intent(UserSetting.this, NotLoginUser.class);
+                    //跳转
+                    startActivity(intent);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 }
