436dd0a6d37cafbc8ddc872dbd9ddd37e6f59e10
ZouyqGitHub <1668751790@qq.com>
2021-08-30 11:17:52 +0800

    push 8.30 01  消息通知设置页面实现 静态关于我们实现

--- app/src/main/java/com/example/myfirstapp/UserSetting.java
+++ app/src/main/java/com/example/myfirstapp/UserSetting.java
@@ -6,18 +6,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.myfirstapp.entity.User;
-import com.example.myfirstapp.util.Notice;
 
 import java.io.FileOutputStream;
+
+import static android.provider.Settings.EXTRA_APP_PACKAGE;
 
 public class UserSetting extends AppCompatActivity {
 
@@ -30,6 +30,8 @@ public class UserSetting extends AppCompatActivity {
         accountInformation();
         communityActivity();
         general();
+        alerts();
+        aboutUs();
     }
 
     @Override
@@ -161,17 +163,92 @@ public class UserSetting extends AppCompatActivity {
         });
     }
 
+    //关于我们
+    private void aboutUs(){
+        //点击事件
+        RelativeLayout relativeLayout = findViewById(R.id.g_y_w_m);
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //通用页面跳转设置
+                Intent intent = new Intent(UserSetting.this, aBoutUs.class);
+                //跳转
+                startActivity(intent);
+            }
+        });
+    }
+
     //新消息通知
-//    private void alerts(){
-//        //获取点击事件
-//        RelativeLayout relativeLayout = findViewById(R.id.x_x_x_t_z);
-//        relativeLayout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-////                Notice notice = new Notice();
-////                notice.sendSubscribeMsg(view);
+    private void alerts() {
+        //获取点击事件
+        RelativeLayout relativeLayout = findViewById(R.id.x_x_x_t_z);
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    // 根据isOpened结果，判断是否需要提醒用户跳转AppInfo页面，去打开App通知权限
+                    Intent intent = new Intent();
+                    intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+                    //这种方案适用于 API 26, 即8.0（含8.0）以上可以用
+                    intent.putExtra(EXTRA_APP_PACKAGE, getPackageName());
+//                    intent.putExtra(EXTRA_CHANNEL_ID, getApplicationInfo().uid);
+
+                    //这种方案适用于 API21——25，即 5.0——7.1 之间的版本可以使用
+                    intent.putExtra("app_package", getPackageName());
+                    intent.putExtra("app_uid", getApplicationInfo().uid);
+
+                    // 小米6 -MIUI9.6-8.0.0系统，是个特例，通知设置界面只能控制"允许使用通知圆点"——然而这个玩意并没有卵用，我想对雷布斯说：I'm not ok!!!
+                    //  if ("MI 6".equals(Build.MODEL)) {
+                    //      intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    //      Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    //      intent.setData(uri);
+                    //      // intent.setAction("com.android.settings/.SubSettings");
+                    //  }
+                    startActivity(intent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    // 出现异常则跳转到应用设置界面：锤子坚果3——OC105 API25
+                    Intent intent = new Intent();
+                    //下面这种方案是直接跳转到当前应用的设置界面。
+                    //https://blog.csdn.net/ysy950803/article/details/71910806
+                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+
+    //获取通知是否开启的权限
+//    private fun initClickListener() {
+//        tv_msg.setOnClickListener {
+//            val intent: Intent = Intent()
+//            try {
+//                intent.action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
+//
+//                //8.0及以后版本使用这两个extra.  >=API 26
+//                intent.putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+//                intent.putExtra(Settings.EXTRA_CHANNEL_ID, applicationInfo.uid)
+//
+//                //5.0-7.1 使用这两个extra.  <= API 25, >=API 21
+//                intent.putExtra("app_package", packageName)
+//                intent.putExtra("app_uid", applicationInfo.uid)
+//
+//                startActivity(intent)
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//
+//                //其他低版本或者异常情况，走该节点。进入APP设置界面
+//                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+//                intent.putExtra("package", packageName)
+//
+//                //val uri = Uri.fromParts("package", packageName, null)
+//                //intent.data = uri
+//                startActivity(intent)
 //            }
-//        });
+//        }
 //    }
 
     //跳转用户页面
